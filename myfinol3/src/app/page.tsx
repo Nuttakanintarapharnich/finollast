0fc1f16d0e5b31a5af4001806a497195e2c54053
Nuttakanintarapharnich <98374208+Nuttakanintarapharnich@users.noreply.github.com>
2023-10-26 14:19:22 +0700

    ...

---
+++ myfinol3/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>
+"คุณอยากรู้ความลับอันยิ่งใหญ่ใหมสมัครเเละล็อกอินเพื่อล่วงรู้ความจริงอันเป็นนิรันดร์"  </h1>
+    </main>
+  );
+}
